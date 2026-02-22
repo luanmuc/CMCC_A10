0a69b44718f1610d8b37b487c0b782e0f38a0cdc
kkk <190509143+luanmuc@users.noreply.github.com>
2026-02-22 14:34:00 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-# 插件源
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-# Argon 主题
-git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git -b 18.06 package/luci-theme-argon
+git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git -b 18.06 package/luci-app-argon-config
 
-# 插件商店
 git clone --depth=1 https://github.com/linkease/istore.git package/istore
 git clone --depth=1 https://github.com/linkease/luci-app-store.git package/luci-app-store
