c1f44b21e6cce5e5f687876c521aab67350d47f4
kkk <190509143+luanmuc@users.noreply.github.com>
2026-02-22 15:23:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# 插件源（适配 immortalwrt，改用 fastgit 镜像）
-sed -i '$a src-git kenzo https://hub.fastgit.xyz/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://hub.fastgit.xyz/kenzok8/small' feeds.conf.default
+#插件源（you you you GitHub）
+sed（流编辑器）you you you-i'$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed（流编辑器）you you you-i'$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-# Argon 主题 (使用 fastgit 镜像克隆)
-git clone --depth=1 https://hub.fastgit.xyz/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone --depth=1 https://hub.fastgit.xyz/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+#Argon主题（你市长you GitHub）
+git克隆-深度=1https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git克隆-深度=1https://github.com/jerrykuku/luci-app-argon-config.git包/luci-app-argon-config
 
-# 插件商店 (使用 fastgit 镜像克隆)
-git clone --depth=1 https://hub.fastgit.xyz/linkease/istore.git package/istore
-git clone --depth=1 https://hub.fastgit.xyz/linkease/luci-app-store.git package/luci-app-store
+#插件商店（悠悠GitHub）
+git克隆-深度=1https://github.com/linkease/istore.git package/istore
+git克隆-深度=1https://github.com/linkease/luci-app-store.git package/luci-app-store
