e13bb3079f68bcfcc0de4bfce3a80f2a9a67e358
kkk <190509143+luanmuc@users.noreply.github.com>
2026-02-22 15:06:49 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# 插件源（适配 immortalwrt）
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+# 插件源（适配 immortalwrt，改用镜像）
+sed -i '$a src-git kenzo https://github.com.cnpmjs.org/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com.cnpmjs.org/kenzok8/small' feeds.conf.default
 
-# Argon 主题 (使用master分支以保证兼容性)
-git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+# Argon 主题 (使用 CNPM 镜像克隆，避免认证错误)
+git clone --depth=1 https://github.com.cnpmjs.org/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone --depth=1 https://github.com.cnpmjs.org/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 
-# 插件商店
-git clone --depth=1 https://github.com/linkease/istore.git package/istore
-git clone --depth=1 https://github.com/linkease/luci-app-store.git package/luci-app-store
+# 插件商店 (使用 CNPM 镜像克隆，避免认证错误)
+git clone --depth=1 https://github.com.cnpmjs.org/linkease/istore.git package/istore
+git clone --depth=1 https://github.com.cnpmjs.org/linkease/luci-app-store.git package/luci-app-store
